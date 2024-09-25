e105e9d6ae13554169035d5a8d11604e636ed104
Anil khand <anilkhandthakuri@gmail.com>
2024-09-25 13:56:27 +1000

    Updates

---
+++ precache-manifest.e962d81ea88ffe45a681875c4a013a65.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cc5e0e94e62d73d26d7b1a582f7455",
+    "url": "/react-nottodolist/index.html"
+  },
+  {
+    "revision": "41e82ecf263e82eff328",
+    "url": "/react-nottodolist/static/css/2.3f403da0.chunk.css"
+  },
+  {
+    "revision": "d4ed695465cd28a32f49",
+    "url": "/react-nottodolist/static/css/main.c2f02618.chunk.css"
+  },
+  {
+    "revision": "41e82ecf263e82eff328",
+    "url": "/react-nottodolist/static/js/2.7b8c3cd7.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/react-nottodolist/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "d4ed695465cd28a32f49",
+    "url": "/react-nottodolist/static/js/main.d1cfd826.chunk.js"
+  },
+  {
+    "revision": "d10315300454245c5942",
+    "url": "/react-nottodolist/static/js/runtime~main.2d88d391.js"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/react-nottodolist/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/react-nottodolist/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/react-nottodolist/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/react-nottodolist/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/react-nottodolist/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/react-nottodolist/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/react-nottodolist/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/react-nottodolist/static/media/fa-v4compatibility.7c428138.woff2"
+  }
+]);
